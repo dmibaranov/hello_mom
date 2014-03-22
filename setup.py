786ec2a999d830759056a458ba7558227a56e5bb
Dmi Baranov <dmi.baranov@gmail.com>
2014-03-22 21:09:23 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
-#my God!
-#pystache
-#PIL
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hello_mom',
+    version='0.1',
+    description='Generate simple static photo album',
+    author='Dmi Baranov',
+    author_email='dmi.baranov@gmail.com',
+    packages=find_packages(),
+    install_requires=['Pillow', 'pystache'],
+)
